a4ef2b0314d5ea01bdd0495b0f66081807f98de1
Roger Walker <roger.john.walker@gmail.com>
2014-11-15 11:01:12 -0500

    Chapter 3: Adding debug messages, landscape activity xml and saving state to activity record, so the rotation doesn't cause us to forget what question we're on.

--- app/src/main/java/com/blooot/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/blooot/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.blooot.android.geoquiz;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,10 @@ import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
+
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private enum Direction{
         NONE,
@@ -39,11 +44,16 @@ public class QuizActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        // Set the first question
-        updateQuestion(Direction.NONE);
+
+        // Set the first question, or the saved state question.
+        updateQuestion(Direction.NONE, savedInstanceState);
+
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,8 +101,54 @@ public class QuizActivity extends ActionBarActivity {
 
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 
-    private void updateQuestion(Direction direction) {
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onStart();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onStart();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStart();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onStart();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    private  void updateQuestion(Direction direction)
+    {
+        updateQuestion(direction, null);
+    }
+
+    private void updateQuestion(Direction direction, Bundle savedState) {
         switch (direction)
         {
             case PREV:
@@ -108,6 +164,8 @@ public class QuizActivity extends ActionBarActivity {
                 mCurrentIndex = (mCurrentIndex+1)% mQuestionBank.length;
                 break;
             case NONE:
+                mCurrentIndex = (savedState != null) ? savedState.getInt(KEY_INDEX, 0) : 0;
+                break;
             default:
                 mCurrentIndex = 0;
                 break;
@@ -122,7 +180,7 @@ public class QuizActivity extends ActionBarActivity {
     {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
-        int resultId = 0;
+        int resultId;
         if (userPressedTrue == answerIsTrue)
         {
             resultId = R.string.correct_toast;
