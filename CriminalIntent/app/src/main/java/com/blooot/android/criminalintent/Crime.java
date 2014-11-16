13ef972ad128693b48c3dbec8cf0a106d718c969
Roger Walker <roger.john.walker@gmail.com>
2014-11-16 15:25:53 -0500

    Chapter 8: Adding some styled TextViews, using graphical desginer.

--- CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/Crime.java
@@ -1,17 +1,37 @@
 package com.blooot.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by rjw on 11/16/2014.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 
     public UUID getId() {
         return mId;
     }
-
-    private UUID mId;
 
     public String getTitle() {
         return mTitle;
@@ -21,11 +41,11 @@ public class Crime {
         mTitle = title;
     }
 
-    private String mTitle;
 
     public Crime() {
         // Generate unique identifier for the Crime!
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
 }
