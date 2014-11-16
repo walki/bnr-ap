aad78c9151d47bfb26c4eb64c7539ada89c0bcf6
Roger Walker <roger.john.walker@gmail.com>
2014-11-16 15:36:16 -0500

    Chap 8 Challenge: More readable data format

--- CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/CrimeFragment.java
@@ -13,6 +13,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.text.DateFormat;
+
 /**
  * Created by rjw on 11/16/2014.
  */
@@ -53,7 +55,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(DateFormat.getDateInstance().format(mCrime.getDate()));
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
