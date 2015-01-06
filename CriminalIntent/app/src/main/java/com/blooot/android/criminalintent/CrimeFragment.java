7169a773c4176cf4033b4eae3fb1ba39d4250944
Roger Walker <roger.john.walker@gmail.com>
2015-01-06 08:58:36 -0500

    Chap 20: 2nd half

--- CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,6 +25,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Date;
 import java.util.UUID;
@@ -38,6 +40,8 @@ public class CrimeFragment extends Fragment {
     private static final String DIALOG_DATE = "date";
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_PHOTO = 1;
+    private static final String DIALOG_IMAGE = "image";
+
 
 
 
@@ -46,6 +50,7 @@ public class CrimeFragment extends Fragment {
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
 
     // Call this to create new CrimeFragments instead of the default constructor
@@ -150,6 +155,20 @@ public class CrimeFragment extends Fragment {
         });
 
 
+        mPhotoView = (ImageView)v.findViewById(R.id.crime_imageView);
+        mPhotoView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Photo p = mCrime.getPhoto();
+                if (p == null)
+                    return;
+
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+                ImageFragment.newInstance(path).show(fm, DIALOG_IMAGE);
+            }
+        });
+
+
         // If the camera DNE then disable the button
         PackageManager pm = getActivity().getPackageManager();
         boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
@@ -173,13 +192,16 @@ public class CrimeFragment extends Fragment {
             mCrime.setDate(date);
             updateDate();
         }else if (requestCode == REQUEST_PHOTO){
+
             // Create a new Photo object and attach it to the crime
             String filename = data.getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
+
             if (filename != null){
-                //Log.i(TAG, "receievd filename from intent: " + filename);
+
                 Photo p = new Photo(filename);
                 mCrime.setPhoto(p);
-                Log.i(TAG, "Crime:  " + mCrime.getTitle() + " has a photo");
+                showPhoto();
+                //Log.i(TAG, "Crime:  " + mCrime.getTitle() + " has a photo");
             }
         }
     }
@@ -192,5 +214,28 @@ public class CrimeFragment extends Fragment {
     public void onPause() {
         super.onPause();
         CrimeLab.get(getActivity()).saveCrimes();
+    }
+
+    private void showPhoto(){
+        // (Re)set the image button's image based on our photo
+        Photo p = mCrime.getPhoto();
+        BitmapDrawable b = null;
+        if (p != null){
+            String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+            b = PictureUtils.getScaledDrawable(getActivity(), path);
+        }
+        mPhotoView.setImageDrawable(b);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showPhoto();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        PictureUtils.cleanImageView(mPhotoView);
     }
 }
---
+++ CriminalIntent/app/src/main/java/com/blooot/android/criminalintent/ImageFragment.java
@@ -0,0 +1,50 @@
+package com.blooot.android.criminalintent;
+
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by rjw on 1/6/2015.
+ */
+public class ImageFragment extends DialogFragment {
+
+    public static final String EXTRA_IMAGE_PATH =  "com.blooot.android.criminalintent.image_path";
+
+    private ImageView mImageView;
+
+    public static ImageFragment newInstance(String imagePath){
+
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_IMAGE_PATH, imagePath);
+
+        ImageFragment fragment = new ImageFragment();
+        fragment.setArguments(args);
+        fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mImageView = new ImageView(getActivity());
+        String path = (String)getArguments().getSerializable(EXTRA_IMAGE_PATH);
+        BitmapDrawable image = PictureUtils.getScaledDrawable(getActivity(), path);
+        mImageView.setImageDrawable(image);
+
+        return mImageView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        PictureUtils.cleanImageView(mImageView);
+    }
+}
